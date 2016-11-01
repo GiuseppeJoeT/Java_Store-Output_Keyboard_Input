b35d8cb2bae6ff19ac00ee0fcd5efbdc588dfccb
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-11-01 18:19:37 +0100

    README and VersatileSnitSoft.java update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Java project:  How Store and Output Keyboard Input
 
 In this little project I show you how we can read decimal numbers from the Keyboard, store them in a variable and print them out on the screen using the **Scanner** class (Java API 5.0 and later).
+
+
+## Acknowledgments
+The code in this project is provided by the book **Beginning Programming with Java® For Dummies®, 4th Edition** written by **Barry Burd**.
--- src/VersatileSnitSoft.java
+++ src/VersatileSnitSoft.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class VersatileSnitSoft {
 
 	public static void main(String args[]) {
-		Scanner keyboard = new Scanner(System.in);
+		Scanner tastiera = new Scanner(System.in);
 		double amount;
 		
 		System.out.print("What's the price of a CD-ROM? ");
 		
-		amount = keyboard.nextDouble();
+		amount = tastiera.nextDouble();
 		// amount gets the value from the User input
 		
 		amount = amount + 25;
@@ -17,7 +17,7 @@ public class VersatileSnitSoft {
 		System.out.print(amount);
 		System.out.println(" to your credit card.");
 		
-		keyboard.close();
+		tastiera.close();
 		
 	}
 }
